47db7c59a20d4ba6d5df31b2af04d1cb7a1bfa86
alex <alex@cloudware.it>
2012-02-19 16:25:49 +0100

    using module's __version__; moving to code.google.com/p/gae-simpleauth

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@
 import os
 from distutils.core import setup
 
+import simpleauth
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='simpleauth',
-      version='0.1.1',
+      version=simpleauth.__version__,
       author='Alex Vagin (http://alex.cloudware.it)',
-      author_email='alex at cloudware.it',
-      url='http://simpleauth.appspot.com',
-      download_url='https://github.com/crhym3/simpleauth/zipball/master',
+      author_email='alex@cloudware.it',
+      url='http://code.google.com/p/gae-simpleauth',
+      download_url='http://code.google.com/p/gae-simpleauth/source/checkout',
       description='A simple auth handler for Google App Engine supporting OAuth 1.0a, 2.0 and OpenID',
       keywords='oauth oauth2 openid appengine google',
       platforms = ["any"],
       license='MIT',
-      requires=['lxml', 'oauth2'],
-      provides=['simpleauth'],
+      requires=['lxml', 'oauth2', 'httplib2'],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -29,6 +30,6 @@ setup(name='simpleauth',
         'Topic :: Security',
         'Topic :: Software Development :: Libraries',    
       ],
-      packages=['simpleauth', 'tests', 'example'],
+      packages=['simpleauth'],
       long_description=read('README')
 )
--- simpleauth/__init__.py
+++ simpleauth/__init__.py
@@ -1,6 +1,6 @@
 """A simple auth handler for Google App Engine supporting OAuth 1.0a, 2.0 and OpenID."""
 
-__version__ = '0.1'
+__version__ = '0.1.2'
 __license__ = "MIT"
 __author__ = "Alex Vagin (http://alex.cloudware.it)"
 __copyright__ = "Copyright 2012, Alex Vagin"
