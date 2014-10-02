4cae48bb0743fec41a4ec297ba4a748ac54e525a
alex <alex@cloudware.it>
2014-10-02 16:38:09 +0100

    Bump version to 0.1.5

--- simpleauth/__init__.py
+++ simpleauth/__init__.py
@@ -3,7 +3,7 @@ A simple auth handler for Google App Engine supporting
 OAuth 1.0a, 2.0 and OpenID.
 """
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __license__ = 'MIT'
 __author__ = 'Alex Vaghin (alex@cloudware.it)'
 
